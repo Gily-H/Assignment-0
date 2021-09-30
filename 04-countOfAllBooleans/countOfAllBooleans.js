8632b7c2d188d645f67afaf4f0f840e6967d1a03
Gilman Huang <gilman.huang213@gmail.com>
2021-09-30 18:40:48 -0400

    complete task-04-countOfAllBooleans

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+	let booleanCount = 0;
+
+  // for each element, check for strict equality with true or false
+  for (const element of arr) {
+    // strict equality, "true" and "false" as string data types are not counted
+    if (element === true || element === false) {
+      booleanCount++;
+    }
+  }
+
+	return booleanCount;
 }
 
 // Do not edit this line;
