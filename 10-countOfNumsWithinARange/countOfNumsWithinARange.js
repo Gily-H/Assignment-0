be94772cf64feca527ebe34dbfa19173bd54303b
Gilman Huang <gilman.huang213@gmail.com>
2021-10-03 15:51:45 -0400

    complete 10-countOfNumsWithinRange

--- 10-countOfNumsWithinARange/countOfNumsWithinARange.js
+++ 10-countOfNumsWithinARange/countOfNumsWithinARange.js
@@ -1,5 +1,14 @@
 function countOfNumsWithinARange(nums, start, end) {
-	// Insert code here;
+	let numsInRangeCount = 0;
+
+	for (const number of nums) {
+		// increment count if number is in range from start to end, both inclusive
+		if (number >= start && number <= end) {
+			numsInRangeCount++;
+		}
+	}
+
+	return numsInRangeCount;
 }
 
 // Do not edit this line;
