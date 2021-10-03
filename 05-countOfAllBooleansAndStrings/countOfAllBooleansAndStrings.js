23bc0c3d1facb52c734ab83c1038fbbdb111773e
Gilman Huang <gilman.huang213@gmail.com>
2021-10-03 13:48:05 -0400

    complete task-05-countOfAllBooleansAndStrings

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+	let booleanAndStringCount = 0;
+
+	for (const element of arr) {
+		// typeof checks the element's data type
+		if (element === true || element === false || typeof element === "string") {
+			booleanAndStringCount++;
+		}
+	}
+
+	return booleanAndStringCount;
 }
 
 // Do not edit this line;
