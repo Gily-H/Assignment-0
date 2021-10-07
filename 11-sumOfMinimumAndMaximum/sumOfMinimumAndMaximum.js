3f365c5f76ed03910e7ca4868f6f188311c3cae4
Gilman Huang <gilman.huang213@gmail.com>
2021-10-06 21:06:05 -0400

    complete 11-sumOfMinimumAndMaximum

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,19 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  // assuming the array nums is not sorted, 
+  // need to loop through entire array
+
+  let minNum = nums[0];
+  let maxNum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < minNum) {
+      minNum = nums[i];
+    } else if (nums[i] > maxNum) {
+      maxNum = nums[i];
+    }
+  }
+  
+  return minNum + maxNum;
 }
 
 // Do not edit this line;
