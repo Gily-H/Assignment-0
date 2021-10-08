e27818532b511546cb9d4c0a7d41fad72beb4113
Gilman Huang <gilman.huang213@gmail.com>
2021-10-08 12:14:55 -0400

    complete 13-isPalindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,17 @@
+// palindrome -> letters from start-middle === letters from end-middle
 function isPalindrome(word) {
-  // Insert code here;
+
+  // loop through letters in first half of the word and compare 
+  // with letters of second half starting from the last letter going to middle letter
+  for (let i = 0; i < word.length / 2; i++) {
+
+    // if any letters do not match, not a palindrome
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // Do not edit this line;
