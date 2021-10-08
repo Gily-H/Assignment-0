60c1cfe466dff3fe9bc6221a9c07006947c179d7
Gilman Huang <gilman.huang213@gmail.com>
2021-10-08 13:06:20 -0400

    complete 14-frequencyCounter

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let freqTracker = {};
+
+  for (const letter of word) {
+
+    // if letter already present, increment frequency, else add new letter with freq 1
+    freqTracker.hasOwnProperty(letter) ? freqTracker[letter]++ : freqTracker[letter] = 1;
+    console.log(freqTracker);
+  }
+
+  return freqTracker;
 }
 
 // Do not edit this line;
